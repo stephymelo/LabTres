4378bff245a9a3066f6553c21bb91e3ede660f3d
Cryptx <steph@Stephanies-MacBook-Air.local>
2020-02-07 15:36:24 -0500

    cambio en el main

--- Main.js
+++ Main.js
@@ -1,16 +1,42 @@
+let matriz = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0],
+];
+
+
+let ancho;
+let alto;
+
+
+let fila;
+let col;
 
 function setup(){
     createCanvas(500,500);
-
-  
+ fila=0;
+ col=0;
+ ancho=70;
+ alto=70;
 }
 
 
 function draw (){
    background(125,0,0);
+
+   for (fila = 0; fila < 3; fila++) {
+    for (col = 0; col < 3; col++) {
+        if (matriz[fila][col] == 0) {
+            rect(0+(fila*alto),0+(col*ancho),ancho,alto);
+            noFill();
+            
+         } else {
+            
+            
+            }
     
 }
-
+   }
 
 
 function mousePressed(){
@@ -19,3 +45,4 @@ function mousePressed(){
    
 }
 
+}
